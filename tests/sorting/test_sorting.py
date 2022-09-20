53143206746d753859df01544276890c4e6e0cd1
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-09-20 11:07:28 -0300

    requisito 12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,10 @@
-# from src.sorting import sort_by
+from src.jobs import read
+from src.sorting import sort_by
+
+jobs = read("src/jobs.csv")
 
 
 def test_sort_by_criteria():
-    pass
+    
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["min_salary"] == "19857"
