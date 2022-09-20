d99011ddb6e3b17a8e99d84b689e034098159c88
Aryeh I. Braid David <aryehbdavid@gmail.com>
2022-09-20 10:56:00 -0300

    requisito 4

--- src/insights.py
+++ src/insights.py
@@ -57,21 +57,13 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    jobs_list = read(path)
+    salaries_list = [
+        int(job["max_salary"])
+        for job in jobs_list if job["max_salary"].isnumeric()
+    ]
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    return max(salaries_list)
 
 
 def get_min_salary(path):
